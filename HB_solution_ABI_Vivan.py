bc18d06b70a3427b328f6792e2f2742d94fff69d
henrikh.baghramyan@gmail.com <35291491+chester1986@users.noreply.github.com>
2022-08-26 16:55:14 +0200

    importing tabulated file

--- HB_solution_ABI_Vivan.py
+++ HB_solution_ABI_Vivan.py
@@ -3,9 +3,10 @@ import pandas as pd
 import json
 from sqlalchemy import create_engine
 import mysql.connector
+import numpy as np
 
-# First part: creating mysql db and inserting into it tables "Genes to down regulate" and Genes to up regulate
-# for each patient
+print("\nFirst part: creating mysql db and inserting into it tables **Genes to down regulate** and **Genes to up "
+      "regulate** for each patient.")
 
 # importing benchling_entries.json to python
 with open('benchling_entries.json', 'r') as f:
@@ -52,16 +53,18 @@ for patients in bench_entries['entries']: # looping over all patients
                 genes['GUR'] = gen_list
     pat[name] = genes # putting genes dictionary under the name of a particular patient
 
-# printing all patient genes
+print('Printing all patient genes.\n')
 for pat_name in pat.keys():
     print(f"For patient {pat_name} data is\n", pd.DataFrame.from_dict(pat[pat_name], orient='index').transpose())
 
-# Credentials to database connection
+# Credentials for database connection
 hostname = "localhost"
 dbname = "patients_db"
 uname = "hb_user"
 pwd = "ABI_Vivan"
 
+print("\nWe can see that for patiend 'Pat012' there is no data for GUR. We will check this in MySQL too.")
+print(f"\nFor patient Pat012 data is\n", pd.DataFrame.from_dict(pat["Pat012"], orient='index').transpose())
 
 # creating an MySQL database 
 
@@ -84,3 +87,26 @@ for k in pat.keys():
     df = pd.DataFrame.from_dict(pat[k], orient='index').transpose()
     df.to_sql(k.lower(), engine, index=False, if_exists='replace')
 
+print("\nSecond part: processing 'cnv_procerssed.txt' and inserting it into a 'cnv' table of the patients_db database.")
+
+# reading 'cnv_processed.txt' as a pandas dataframe
+cnv = pd.read_csv('cnv_processed.txt', sep='\t', lineterminator='\r', low_memory=False);
+
+# Visualization of the dataframe
+cnv.head()
+
+# printing all patient names in 'cnv_processed.txt'
+print("\nPatient names present in the 'cnv_processed.txt':", cnv.Patient_ID.unique())
+
+print("\nChecking if 'cnv_processed.txt' has empty strings in it.")
+print(np.where(cnv.applymap(lambda x: x == '')))
+print("It seems there is no such patient.")
+
+print("\nChecking if there is a patient with non NaN in any row.")
+nonNaNpatients = cnv.dropna().Patient_ID.unique()
+print("Patient(s) with no NaN in any row:", np.ravel(nonNaNpatients))
+print(f"It seems the only {np.ravel(nonNaNpatients)} has rows of data with no missing values (NaNs). "
+      f"We will check this in MySQL too.")
+
+# Inserting cnv as a table into patients_db.
+cnv.to_sql("cnv", engine, index=True, if_exists='replace');
